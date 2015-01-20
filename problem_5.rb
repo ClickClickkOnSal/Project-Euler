13ce4b034ea2539e3086bb268b44ac5949e84d22
ClickClickkOnSal <s.hernandez5400@gmail.com>
2015-01-20 01:20:28 -0500

    solved problem 5 with some help of the internet

--- problem_5.rb
+++ problem_5.rb
@@ -2,3 +2,23 @@
 
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
+def find_multiple
+  lcm = 1
+
+  (2..20).each do |i|
+    lcm *= i / gcd(lcm, i)
+  end
+
+  lcm
+end
+
+def gcd(a, b)
+  while b > 0
+    a %= b
+    return b if a == 0
+    b %= a
+  end
+  a
+end
+
+puts find_multiple
