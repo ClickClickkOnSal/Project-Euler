88e793fc30cfd7f9b56f2f618632826b9b2f1979
ClickClickkOnSal <s.hernandez5400@gmail.com>
2015-02-13 16:04:50 -0500

    solved problem 9 with the internet

--- problem_9.rb
+++ problem_9.rb
@@ -6,3 +6,27 @@
 
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
+
+# Euclid's formula yields Pythagorean triples for integers m and n with m < n: 
+# a = m**2 - n**2 ; b = 2*m*n ; c = m**2 + n**2
+
+x = 1000
+
+def euclids upto 
+  result = []
+  (2..upto).each do |m| # Start at 2 as 1 results nothing anyway
+    (1...m).each do |n| # Euclid's formula only works for m > n
+      result << [m**2 - n**2, 2*m*n, m**2 + n**2]
+    end
+  end
+  result
+end
+
+# Calling Euclids on n/2 suffices as m + n < x
+euclids(x/2).each do |triple|
+  a, b, c = triple[0], triple[1], triple[2]
+  if a + b + c == x
+    puts a * b * c
+    break
+  end
+end
