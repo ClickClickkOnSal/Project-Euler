e12da4195757ac1fc8bb517b85087618106cdddd
ClickClickkOnSal <s.hernandez5400@gmail.com>
2015-02-15 16:30:33 -0500

    solved problem 10

--- problem_10.rb
+++ problem_10.rb
@@ -1,3 +1,12 @@
+require "mathn"
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 # Find the sum of all the primes below two million.
+
+prime_sum = 0
+
+Prime.each(2_000_000) do |prime|
+  prime_sum += prime
+end
+
+puts prime_sum
